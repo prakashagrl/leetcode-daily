0452404aa25f8efbb22e0aa693705d87f09d7b3a
Prakash Agrawal <prakashagrl.2004@gmail.com>
2026-08-24 21:15:11 +0530

    Solve LeetCode #1 - Two Sum

---
+++ 2026/August/24-two-sum/solution.cpp
@@ -0,0 +1,16 @@
+// LeetCode #1 - Two Sum
+// Difficulty: Easy
+// Date: 24 August 2026
+// Language: C++
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution
+{
+public:
+       vector<int> twoSum(vector<int> &nums, int target)
+       {
+              // Your solution
+       }
+};
